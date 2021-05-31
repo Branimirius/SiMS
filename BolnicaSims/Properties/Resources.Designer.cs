56f6cf55b14a8be2f0aae7217498d933469bda47
ra149 <fresh.trifunovic@gmail.com>
2021-05-31 22:28:17 +0200

    Uradjena cela lokalizacija pacijenta

--- BolnicaSims/Properties/Resources.Designer.cs
+++ BolnicaSims/Properties/Resources.Designer.cs
@@ -61,6 +61,24 @@ namespace BolnicaSims.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Alergies.
+        /// </summary>
+        internal static string Alergije {
+            get {
+                return ResourceManager.GetString("Alergije", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Anamnesis.
+        /// </summary>
+        internal static string Anamneza {
+            get {
+                return ResourceManager.GetString("Anamneza", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Hospital survey.
         /// </summary>
         internal static string AnketaBolnica {
@@ -75,6 +93,15 @@ namespace BolnicaSims.Properties {
         internal static string AnketaDoktor {
             get {
                 return ResourceManager.GetString("AnketaDoktor", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Note.
+        /// </summary>
+        internal static string Beleska {
+            get {
+                return ResourceManager.GetString("Beleska", resourceCulture);
             }
         }
         
@@ -97,6 +124,24 @@ namespace BolnicaSims.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Date.
+        /// </summary>
+        internal static string Datum {
+            get {
+                return ResourceManager.GetString("Datum", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Date and time.
+        /// </summary>
+        internal static string DatumVreme {
+            get {
+                return ResourceManager.GetString("DatumVreme", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Doctor.
         /// </summary>
         internal static string Doktor {
@@ -106,11 +151,29 @@ namespace BolnicaSims.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Age.
+        /// </summary>
+        internal static string Godine {
+            get {
+                return ResourceManager.GetString("Godine", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Id.
         /// </summary>
         internal static string IdTermina {
             get {
                 return ResourceManager.GetString("IdTermina", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Name.
+        /// </summary>
+        internal static string Ime {
+            get {
+                return ResourceManager.GetString("Ime", resourceCulture);
             }
         }
         
@@ -160,6 +223,15 @@ namespace BolnicaSims.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Comment.
+        /// </summary>
+        internal static string Komentar {
+            get {
+                return ResourceManager.GetString("Komentar", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Ends at.
         /// </summary>
         internal static string KrajTermina {
@@ -196,11 +268,92 @@ namespace BolnicaSims.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Subject:.
+        /// </summary>
+        internal static string Naslov {
+            get {
+                return ResourceManager.GetString("Naslov", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to NOTIFICATIONS.
+        /// </summary>
+        internal static string Notifikacije {
+            get {
+                return ResourceManager.GetString("Notifikacije", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Grade(1-10).
+        /// </summary>
+        internal static string Ocena {
+            get {
+                return ResourceManager.GetString("Ocena", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Cancel appointment.
         /// </summary>
         internal static string Otkazi {
             get {
                 return ResourceManager.GetString("Otkazi", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gender.
+        /// </summary>
+        internal static string Pol {
+            get {
+                return ResourceManager.GetString("Pol", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cancel.
+        /// </summary>
+        internal static string Ponisti {
+            get {
+                return ResourceManager.GetString("Ponisti", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sender:.
+        /// </summary>
+        internal static string Posiljalac {
+            get {
+                return ResourceManager.GetString("Posiljalac", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Accept.
+        /// </summary>
+        internal static string Potvrdi {
+            get {
+                return ResourceManager.GetString("Potvrdi", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Surname.
+        /// </summary>
+        internal static string Prezime {
+            get {
+                return ResourceManager.GetString("Prezime", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Priority.
+        /// </summary>
+        internal static string Prioritet {
+            get {
+                return ResourceManager.GetString("Prioritet", resourceCulture);
             }
         }
         
