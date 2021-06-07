cd54a055b752738647c1844c84613bfca92d68dc
Branimirius <73712742+Branimirius@users.noreply.github.com>
2021-06-07 21:52:53 +0200

    state sablon part 1

---
+++ BolnicaSims/Model/ILek.cs
@@ -0,0 +1,12 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BolnicaSims.Model
+{
+    interface ILek
+    {
+        public void setContext(StanjaLeka context) { }
+    }
+}
