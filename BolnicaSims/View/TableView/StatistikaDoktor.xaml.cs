39708b9b5f70cf2f827d4b2cf42fd65547356169
piwneuh <filip.pinjuh@gmail.com>
2021-06-08 21:05:23 +0200

    Chart + Demo bugfix

--- BolnicaSims/View/TableView/StatistikaDoktor.xaml.cs
+++ BolnicaSims/View/TableView/StatistikaDoktor.xaml.cs
@@ -1,5 +1,5 @@
-﻿//using LiveCharts;
-//using LiveCharts.Wpf;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,53 +21,53 @@ namespace BolnicaSims.View.TableView
     public partial class StatistikaDoktor : Page
     {
 
-        //public SeriesCollection SeriesCollection { get; set; }
-        //public string[] Labels { get; set; }
-        //public Func<double, string> YFormatter { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
 
-        //public StatistikaDoktor()
-        //{
-        //    InitializeComponent();
+        public StatistikaDoktor()
+        {
+            InitializeComponent();
 
-        //    SeriesCollection = new SeriesCollection
-        //    {
-        //        new LineSeries
-        //        {
-        //            Title = "Series 1",
-        //            Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-        //        },
-        //        new LineSeries
-        //        {
-        //            Title = "Series 2",
-        //            Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
-        //            PointGeometry = null
-        //        },
-        //        new LineSeries
-        //        {
-        //            Title = "Series 3",
-        //            Values = new ChartValues<double> { 4,2,7,2,7 },
-        //            PointGeometry = DefaultGeometries.Square,
-        //            PointGeometrySize = 15
-        //        }
-        //    };
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
 
-        //    Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-        //    YFormatter = value => value.ToString("C");
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
 
-        //    SeriesCollection.Add(new LineSeries
-        //    {
-        //        Title = "Series 4",
-        //        Values = new ChartValues<double> { 5, 3, 2, 4 },
-        //        LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-        //        PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-        //        PointGeometrySize = 50,
-        //        PointForeground = Brushes.Gray
-        //    });
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
 
-        //    //modifying any series values will also animate and update the chart
-        //    SeriesCollection[3].Values.Add(5d);
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[3].Values.Add(5d);
 
-        //    DataContext = this;
-        //}
+            DataContext = this;
+        }
     }
 }
